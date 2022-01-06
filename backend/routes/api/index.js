d3d9c012b3c8d2f4a73613b5b31c71c8363797d9
Sornam Vairavan <sornamvairavan@outlook.com>
2022-01-06 13:57:13 -0800

    login route updated

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,20 +1,15 @@
-const router = require('express').Router()
 const asyncHandler = require('express-async-handler');
+const router = require('express').Router()
+const sessionRouter = require('./session.js')
+const usersRouter = require('./users.js')
 const { User } = require('../../db/models');
+
+router.use('/session', sessionRouter)
+
+router.use('/users', usersRouter)
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body})
 })
 
 module.exports = router;
-
-
-// fetch('/api/test', {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//     "XSRF-TOKEN": `ff4BD1H2-kHQxyxlnrC6NBC2xl3yhlwSFnsw`
-//   },
-//   body: JSON.stringify({ hello: 'world' })
-// }).then(res => res.json()).then(data => console.log(data));
-
