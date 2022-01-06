3777713e60497562df82ec8603c9b1fd973bb51a
Sornam Vairavan <sornamvairavan@outlook.com>
2022-01-06 13:40:38 -0800

    user authentication complete

---
+++ backend/db/seeders/20220106192735-demo-user.js
@@ -0,0 +1,53 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        fullName: 'Demo',
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        fullName: 'Mary Jones',
+        email: 'maryjones@email.com',
+        username: 'maryjones65',
+        hashedPassword: bcrypt.hashSync('password123!')
+      },
+      {
+        fullName: 'John Gray',
+        email: 'johngray@email.com',
+        username: 'jgray',
+        hashedPassword: bcrypt.hashSync('password123!')
+      },
+      {
+        fullName: 'Edwin Grande',
+        email: 'edwingrande@email.com',
+        username: 'edwingrande',
+        hashedPassword: bcrypt.hashSync('password123!')
+      },
+      {
+        fullName: 'Rajesh Nair',
+        email: 'rajeshnair@email.com',
+        username: 'rajeshnair93',
+        hashedPassword: bcrypt.hashSync('password123!')
+      },
+      {
+        fullName: 'Ruth Vega',
+        email: 'ruthvega@email.com',
+        username: 'ruthvega254',
+        hashedPassword: bcrypt.hashSync('password123!')
+      },
+
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition']}
+    });
+  }
+};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,6 @@
 const router = require('express').Router()
+const asyncHandler = require('express-async-handler');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
   res.json({ requestBody: req.body})
